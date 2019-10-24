42a26150f14fd6d28466795d7288522809119706
Nik <43304085+CryFR@users.noreply.github.com>
2019-10-24 13:57:33 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,16 @@
+import sqlite3
+import rasp_parser
+
+conn = sqlite3.connect('schedule.db')
+cursor = conn.cursor()
+keys, values = zip(*rasp_parser.get_faculties().items())
+cursor.executescript("""DROP TABLE IF EXISTS facults;
+                CREATE TABLE facults(fac_num , fac_name)""")
+for i in range(0,len(keys)):
+    cursor.execute('''REPLACE INTO facults(fac_num, fac_name) VALUES (?, ?)''', (keys[i],values[i]))
+    i += 1
+
+conn.commit()
+
+
+
