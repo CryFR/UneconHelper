8d8973f15b17b60ec52cd10a3816dcdd94c3d27f
Vladislav <54751848+vermoouth@users.noreply.github.com>
2019-11-16 20:42:31 +0300

    Update db_write.py

--- db_write.py
+++ db_write.py
@@ -5,19 +5,8 @@ from constants import MYSQL_IP, MYSQL_USER, MYSQL_PASSWORD, MYSQL_BD_NAME
 
 def fill_faculties():
     keys_fac, values_fac = zip(*schedule_parser.get_faculties().items())
-    sql = "REPLACE INTO faculties (faculty_id, fac_name) VALUES (%s, %s)"
+    sql = "REPLACE INTO faculties (faculty_id, faculty_name_ru, number_of_courses) VALUES (%s, %s, %s)"
     for i in range(0, len(keys_fac)):
-        data = (keys_fac[i], values_fac[i])
+        data = (keys_fac[i], values_fac[i], len(schedule_parser.get_courses(keys_fac[i])))
         cursor.execute(sql, data)
         i += 1
-
-
-if __name__ == '__main__':
-    conn = pymysql.connect(host=MYSQL_IP,
-                           database=MYSQL_BD_NAME,
-                           user=MYSQL_USER,
-                           password=MYSQL_PASSWORD)
-    cursor = conn.cursor()
-    fill_faculties()
-    conn.commit()
-    cursor.close()
