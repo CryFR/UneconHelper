6ff2076e7030e30f540d7a01a22ee817ce9a5fad
Vladislav <54751848+vermoouth@users.noreply.github.com>
2019-11-16 23:15:32 +0300

    Update db_write.py

--- db_write.py
+++ db_write.py
@@ -5,24 +5,32 @@ from constants import MYSQL_IP, MYSQL_USER, MYSQL_PASSWORD, MYSQL_BD_NAME
 
 def fill_faculties():
     keys_fac, values_fac = zip(*schedule_parser.get_faculties().items())
-    sql = "REPLACE INTO faculties (faculty_id, faculty_name_ru, number_of_courses) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO faculties (faculty_id, faculty_name_ru, courses) VALUES (%s, %s, %s)"
     for i in range(0, len(keys_fac)):
-        data = (keys_fac[i], values_fac[i], len(schedule_parser.get_courses(keys_fac[i])))
-        cursor.execute(sql, data)
+        data = (keys_fac[i], values_fac[i], ','.join((schedule_parser.get_courses(keys_fac[i]).keys())))
+        try:
+            # print(data)
+            cursor.execute(sql, data)
+        except Exception as e:
+            print(e)
+
         i += 1
 
 
 def fill_groups(faculty_id, course):
-    sql = "REPLACE INTO `groups` (group_id, group_number, group_speciality_name, group_speciality_code, faculty_id, course) VALUES (%s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO `groups` (group_id, group_number, group_speciality_name, group_speciality_code, faculty_id, course) VALUES (%s, %s, %s, %s, %s, %s)"
     keys_grp, values_grp = zip(*schedule_parser.get_groups(faculty_id, course).items())
     for i in range(0, len(keys_grp)):
         data = (keys_grp[i], values_grp[i][0], values_grp[i][2], values_grp[i][1], faculty_id, course)
-        cursor.execute(sql, data)
+        try:
+            cursor.execute(sql, data)
+        except Exception as e:
+            print(e)
         i += 1
 
 
 def fill_teachers(group_id):
-    sql = "REPLACE INTO teachers (surname_ru, first_name_ru, patronymic_ru) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO teachers (surname_ru, first_name_ru, patronymic_ru) VALUES (%s, %s, %s)"
     group_teachers = set(lesson['teacher'] for lesson in schedule_parser.parse_semester(group_id))
     for teacher in group_teachers:
         data = teacher.split()
@@ -33,7 +41,7 @@ def fill_teachers(group_id):
 
 
 def fill_subjects(group_id):
-    sql = "REPLACE INTO subjects (subject_name_ru) VALUES (%s)"
+    sql = "INSERT INTO subjects (subject_name_ru) VALUES (%s)"
     group_subjects = set(lesson['subject'] for lesson in schedule_parser.parse_semester(group_id))
     for subject in group_subjects:
         subject = subject.partition('(')[0].rstrip()
@@ -43,12 +51,11 @@ def fill_subjects(group_id):
             print(e)
 
 
-if __name__ == '__main__':
-    conn = pymysql.connect(host=MYSQL_IP,
-                           database=MYSQL_BD_NAME,
-                           user=MYSQL_USER,
-                           password=MYSQL_PASSWORD)
-    cursor = conn.cursor()
-    fill_subjects(12244)
-    conn.commit()
-    cursor.close()
+def fill_rooms(group_id):
+    sql = "INSERT INTO rooms (number, building_ru) VALUES (%s, %s)"
+    group_rooms = set([lesson['room'], lesson['building']] for lesson in schedule_parser.parse_semester(group_id))
+    for room in group_rooms:
+        try:
+            cursor.execute(sql, room)
+        except Exception as e:
+            print(e)
