d72152c6d3ac730677da98e1921acdb6a3960d73
Vladislav <54751848+vermoouth@users.noreply.github.com>
2019-11-19 20:05:11 +0300

    Update schedule_parser.py

--- schedule_parser.py
+++ schedule_parser.py
@@ -1,5 +1,6 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import re
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -9,7 +10,7 @@ group_url = 'https://rasp.unecon.ru/raspisanie_grp.php'
 
 def get_soup(url):
     response = urllib.request.urlopen(url)
-    soup = BeautifulSoup(response, 'html.parser')
+    soup = BeautifulSoup(response, 'lxml')
     return soup
 
 
@@ -36,33 +37,46 @@ def get_groups(faculty_num, course):
 
 def parse_semester(group_id):
     soup = get_soup(group_url+f'?g={group_id}&semestr')
+    semester_period= re.findall('\d{2}\.\d{2}\.\d{4}', soup.find('div', class_='rasp').h1.text)
     table = soup.find('table')
-    rows = table.find_all('tr')
-    days = []
-    for element in rows[1:]:
-        content = element.find_all('td')
-        try:
-            days.append({
-                'time': content[1].span.text,
-                'dates': content[1].find('span', {'class': 'dates'}).text,
-                'even_odd': content[1].find('span', {'class': 'even_odd'}).text,
-                'room': content[2].span.text,
-                'building': content[2].find('span', {'class': 'korpus'}).text,
-                'subject': content[3].find('span', {'class': 'predmet'}).text.rpartition('(')[0].rstrip(),
-                'type': content[3].find('span', {'class': 'predmet'}).text.rpartition('(')[2].rstrip(')'),
-                'teacher': content[3].find('span', {'class': 'prepod'}).text
-            })
-        except:
-            try:
-                days.append({
-                    'time': content[1].span.text,
-                    'dates': content[1].find('span', {'class': 'dates'}).text,
-                    'room': content[2].span.text,
-                    'building': content[2].find('span', {'class': 'korpus'}).text,
-                    'subject': content[3].find('span', {'class': 'predmet'}).text,
-                    'type': content[3].find('span', {'class': 'predmet'}).text.rpartition('(')[2].rstrip(')'),
-                    'teacher': content[3].find('span', {'class': 'prepod'}).text
-                })
-            except:
-                pass
-    return days
+    rows = table('tr')
+    lessons = []
+    for row in rows:
+        if row['class'] != ['new_day_border']:
+            if row.find('span', class_='prepod').text != '':
+                teacher = row.find('span', class_='prepod').text
+                teacher_id = re.findall('\d+', row.find('a', {'title': 'Преподаватель'})['href'])
+            else:
+                teacher = teacher_id = ''
+            if row.find('td', class_='aud').find('span', class_='aud').text + row.find('span', class_='korpus').text != '':
+                room = row.find('td', class_='aud').find('span', class_='aud').text
+                building = row.find('span', class_='korpus').text
+            elif row.find('span', class_='prim').text != '':
+                room = row.find('span', class_='prim').text.rpartition(',')[2].strip()
+                building = row.find('span', class_='prim').text.rpartition(',')[0].strip()
+            else:
+                room = building = ''
+            if row.find('span', class_='predmet').find('span', class_='vibor') is None:
+                subject = row.find('span', class_='predmet').text.partition('(')[0].strip()
+                is_chosable = False
+            else:
+                subject = row.find('span', class_='predmet').text.partition('(')[0].strip()[6:]
+                is_chosable = True
+            time = row.find('span', class_='time').text
+            dates = row.find('span', class_='dates').text
+            even_odd = row.find('span', class_='even_odd').text
+            lesson_type = row.find('span', class_='predmet').text.rpartition('(')[2].rstrip(')')
+            lessons.append({'time': time,
+                           'dates': dates,
+                            'even_odd': even_odd,
+                            'room': room,
+                            'building': building,
+                            'subject': subject,
+                            'is_choosable': is_chosable,
+                            'type': lesson_type,
+                            'teacher': teacher,
+                            'teacher_id': teacher_id,
+                            'semester_period': semester_period
+                            })
+    return lessons
+
