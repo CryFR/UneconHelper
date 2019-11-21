a706955c8073f18808535f2dc651c24f91fa8dba
Vladislav <54751848+vermoouth@users.noreply.github.com>
2019-11-21 11:58:02 +0300

    Update markups.py

--- markups.py
+++ markups.py
@@ -1,9 +1,14 @@
-from telebot.types import ReplyKeyboardMarkup as RplMarkup
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from db_api import *
 
 
+def remove():
+    mkp = ReplyKeyboardRemove()
+    return mkp
+
+
 def start():
-    mkp = RplMarkup(True, False)
+    mkp = ReplyKeyboardMarkup(True, False)
     mkp.row('️️Расписание группы')
     mkp.row('Расписание преподавателя')
     mkp.row('Расписание аудитории')
@@ -11,8 +16,63 @@ def start():
 
 
 def faculties():
-    mkp = RplMarkup(True, False)
-    print(get_faculties())
+    mkp = ReplyKeyboardMarkup(True, False)
     for faculty in get_faculties():
-        mkp.row(faculty[0])
+        mkp.row(faculty)
     return mkp
+
+
+def specialities(faculty):
+    mkp = ReplyKeyboardMarkup(True, False)
+    for speciality in get_specialities(faculty):
+        mkp.row(speciality)
+    return mkp
+
+
+def groups(speciality):
+    mkp = ReplyKeyboardMarkup(True, False, row_width=4)
+    buttons = []
+    for group in get_groups(speciality):
+        buttons.append(KeyboardButton(group))
+    mkp.add(*buttons)
+    return mkp
+
+
+def teachers(name):
+    mkp = ReplyKeyboardMarkup(True, False)
+    for teacher in get_teachers(name):
+        mkp.row(teacher)
+    return mkp
+
+
+def buildings():
+    mkp = ReplyKeyboardMarkup(True, False)
+    for _building in get_buildings():\
+        mkp.row(_building)
+    return mkp
+
+
+def room_numbers(number, _building):
+    mkp = ReplyKeyboardMarkup(True, False, row_width=5)
+    buttons = []
+    for room in get_rooms(number, _building):
+        buttons.append(room)
+    mkp.add(*buttons)
+    return mkp
+
+
+def lesson_notifications():
+    mkp = ReplyKeyboardMarkup(True, False)
+    mkp.row('️️За 5 минут', 'За 10 минут')
+    mkp.row('️️За 15 минут', 'Не уведомлять')
+    return mkp
+
+
+def day_notifications():
+    mkp = ReplyKeyboardMarkup(True, False)
+    mkp.row('️️Вечером', 'Утром')
+    mkp.row('Не уведомлять')
+
+
+if __name__ == '__main__':
+    print(building())
