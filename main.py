cf45721eda9ec72c88b45223659276186ff7261a
Vladislav <54751848+vermoouth@users.noreply.github.com>
2019-11-21 11:57:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,15 +22,50 @@ def handle_command(message):
 
 @bot.message_handler(content_types=['text'])
 def handle_command(message):
+    answer = markup = ''
     user_id = message.from_user.id
-    print(user_id)
-    if get_state(user_id) == 0:
-        if message.text == '️️Расписание группы':
-            change_state(user_id, 1)
-            answer = 'Выбери свой факультет, или введи номер группы'
-            bot.send_message(message.from_user.id, answer, reply_markup=markups.faculties())
-    if get_state(user_id) == 1:
-        answer = 'Теперь выбери курс:'
+    state = get_state(user_id)
+    text = message.text
+    print(f'{state} {user_id}: {text}')
+    if state == 0:
+        if text == '️️Расписание группы':
+            answer = 'Выберите свой факультет, или введи номер группы'
+            markup = markups.faculties()
+            state = 10
+        elif text == 'Расписание преподавателя':
+            answer = 'Введите часть имени преподавателя'
+            markup = markups.remove()
+            state = 20
+        elif text == 'Расписание аудитории':
+            answer = 'Выберите задние:'
+            markup = markups.buildings()
+            state = 30
+    elif state == 10:
+        if text in get_faculties():
+            answer = 'Теперь выберите направление'
+            markup = markups.specialities(text)
+            state = 11
+    elif state == 11:
+        if text in get_specialities('%'):
+            answer = 'Найдите свою группу'
+            markup = markups.groups(text)
+            state = 12
+    elif state == 20:
+        answer = 'Выберите преподавателя из списка'
+        markup = markups.teachers(text)
+        state = 21
+    elif state == 30:
+        if text in get_buildings():
+            answer = 'Выберите аудиторию или введите её номер'
+            markup = markups.room_numbers('%', text)
+            state = 32
+    elif state == 12:
+        answer = f'Выбрана группа {text}'
+        markup = markups.lesson_notifications()
+        set_tracking(user_id, 'group', get_group_id(text), True)
+    if answer != '':
+        bot.send_message(message.from_user.id, answer, reply_markup=markup)
+        change_state(user_id, state)
 
 
 if __name__ == '__main__':
@@ -38,49 +73,3 @@ if __name__ == '__main__':
 
 # except Exception as e:
 #     print(e)
-
-    # start_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-    # start_markup.row()
-    #
-    # faculty_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-    # for faculty in schedule_parser.get_faculties():
-    #     faculty_markup.row(schedule_parser.get_faculties()[faculty])
-    #
-    # language_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-    # language_markup.row('English', 'Русский')
-    #
-    # hide_markup = telebot.types.ReplyKeyboardRemove()
-    #
-    #
-    # def stating(user_id):
-    #     state = None
-    #     states = open('Unecon_States.txt')
-    #     for line in states:
-    #         if line[:9] == str(user_id):
-    #             state = int(line[9])
-    #             states.close()
-    #             break
-    #     if state is None:
-    #         states.close()
-    #         states = open('Unecon_States.txt', 'a')
-    #         states.write(str(user_id) + '0' + '\n')
-    #         states.close()
-    #     return state
-    #
-    #
-    # def change_state(new_state, user_id):
-    #     line_of_user = 0
-    #     with open('Unecon_States.txt') as local_state:
-    #         for local_line in local_state:
-    #             if local_line[:9] == str(user_id):
-    #                 break
-    #             else:
-    #                 line_of_user += 1
-    #     local_state = open('Unecon_States.txt')
-    #     lines = local_state.readlines()
-    #     new_line = lines[line_of_user]
-    #     lines[line_of_user] = new_line[:9] + '{}'.format(str(new_state) + '\n')
-    #     local_state.close()
-    #     save_changes = open('Unecon_States.txt', 'w')
-    #     save_changes.writelines(lines)
-    #     save_changes.close()
