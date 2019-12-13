fa393a18165207ca43eeda610e9ad4a386125952
Vladislav <54751848+vermoouth@users.noreply.github.com>
2019-12-13 21:07:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,17 +12,19 @@ bot = telebot.TeleBot(TOKEN, threaded=False)
 
 
 @bot.message_handler(commands=['start', 'reset'])
-def handle_command(message):
+@connect_and_run
+def handle_command(message, cursor):
     user_id = message.from_user.id
     print(user_id, 'started')
-    reset_user(user_id)
+    reset_user(user_id, cursor)
     answer = 'Привет, я могу выводить расписание, а также, если нужно, напоминать о парах.\nИ так, расписание чего нужно выводить?'
     bot.send_message(user_id, answer, reply_markup=markups.start())
-    change_state(user_id, states['start'])
+    change_state(user_id, states['start'], cursor)
 
 
 @bot.message_handler(content_types=['text'])
-def handle_command(message):
+@connect_and_run
+def handle_command(message, cursor):
     print('get')
     start_time = datetime.datetime.now()
     answer = 'Не понял вас'
@@ -30,10 +32,10 @@ def handle_command(message):
     user_id = message.from_user.id
     bot.send_chat_action(user_id, 'typing')
     try:
-        state = get_state(user_id)
+        state = get_state(user_id, cursor)
     except Exception as e:
         print(e)
-        change_state(user_id, states['start'])
+        change_state(user_id, states['start'], cursor)
         state = states['start']
     text = message.text
     print(f'{state} {user_id}: {text} ')
@@ -51,23 +53,23 @@ def handle_command(message):
             markup = markups.buildings()
             state = states['waiting_for_building']
     elif state == states['waiting_for_faculty/group']:
-        if text in get_all_groups():
-            if len(get_trackings(user_id)) == 0:
-                set_tracking(user_id, 'group', get_group_id(text), True, text)
+        if text in get_all_groups(cursor):
+            if len(get_trackings(user_id, cursor)) == 0:
+                set_tracking(user_id, 'group', get_group_id(text, cursor), True, text, cursor)
                 answer = f'Выбрана группа:\n\n{text}\n\nБот может отсылать вам уведомления о предстоящих занятиях, есть три вида уведомлений:\n\n1)Уведомления перед каждой парой - такие уведомления будут приходить за выбранное время до начала пары\n\n2)Уведомление перед первой парой - для первой пары можно выставить отдельное время\n\n3)Уведомление о парах на день - позволяет получать расписание всех занятий на предстоящий учебный день'
                 bot.send_message(user_id, answer, reply_markup=markup)
                 answer = 'Уведомления перед каждой парой:'
                 markup = markups.every_lesson_notification()
                 state = states['every_lesson_notification']
             else:
-                set_tracking(user_id, 'group', get_group_id(text), False, text)
+                set_tracking(user_id, 'group', get_group_id(text, cursor), False, text, cursor)
                 answer = f'''Выбрана группа:\n\n{text}\n\nТеперь в главном меню вы можете найти кнопку для вывода её расписания\n\nУведомления о парах приходят только для главного расписания, сделать расписание главным:\n\nНастройки → Главное расписание'''
                 bot.send_message(user_id, answer, reply_markup=markups.remove())
                 bot.send_chat_action(user_id, 'typing')
-                answer = get_schedule(user_id, main=True)
+                answer = get_schedule(user_id, main=True, cursor=cursor)
                 markup = markups.main()
-        elif text in get_faculties():
-            set_buffer(user_id, text)
+        elif text in get_faculties(cursor):
+            set_buffer(user_id, text, cursor)
             answer = 'Теперь выберите направление'
             markup = markups.specialities(text)
             state = states['waiting_for_speciality']
@@ -75,28 +77,28 @@ def handle_command(message):
             answer = 'Не могу найти такую группу, выберите факультет'
             markup = markups.faculties()
     elif state == states['waiting_for_speciality']:
-        if text in get_specialities('%'):
+        if text in get_specialities('%', cursor):
             answer = 'Найдите свою группу'
-            markup = markups.groups(get_buffer(user_id), text)
+            markup = markups.groups(get_buffer(user_id, cursor), text)
             state = states['waiting_for_faculty/group']
     elif state == states['waiting_for_teacher']:
         if len(text) >= 3:
-            teachers = get_teachers(text)
+            teachers = get_teachers(text, cursor)
             number_of_teachers = len(teachers)
             if number_of_teachers == 1:
-                if len(get_trackings(user_id)) == 0:
-                    set_tracking(user_id, 'group', get_teacher_id(teachers[0]), True, text)
+                if len(get_trackings(user_id, cursor)) == 0:
+                    set_tracking(user_id, 'group', get_teacher_id(teachers[0], cursor), True, text, cursor)
                     answer = f'''Выбран преподаватель:\n\n{teachers[0]}\n\nБот может отсылать вам уведомления о предстоящих занятиях, есть три вида уведомлений:\n\n1)Уведомления перед каждой парой - такие уведомления будут приходить за выбранное время до начала пары\n\n2)Уведомление перед первой парой - для первой пары можно выставить отдельное время\n\n3)Уведомление о парах на день - позволяет получать расписание всех занятий на предстоящий день'''
                     bot.send_message(user_id, answer, reply_markup=markup)
                     answer = 'Уведомления перед каждой парой:'
                     markup = markups.every_lesson_notification()
                     state = states['every_lesson_notification']
                 else:
-                    set_tracking(user_id, 'group', get_teacher_id(text), False, text)
+                    set_tracking(user_id, 'group', get_teacher_id(text, cursor), False, text, cursor)
                     answer = f'''Выбран преподаватель:\n\n{teachers[0]}\n\nТеперь в главном меню вы можете найти кнопку для вывода расписания для данного преподавателя\n\nУведомления о парах приходят только для главного расписания, сделать расписание главным:\n\nНастройки → Главное расписание'''
                     bot.send_message(user_id, answer, reply_markup=markups.remove())
                     bot.send_chat_action(user_id, 'typing')
-                    answer = get_schedule(user_id, main=True)
+                    answer = get_schedule(user_id, main=True, cursor=cursor)
                     markup = markups.main()
             elif number_of_teachers > 1:
                 answer = 'Выберите преподавателя из списка'
@@ -108,38 +110,38 @@ def handle_command(message):
             answer = 'Введите хотя-бы три символа'
             markup = markups.remove()
     elif state == states['waiting_for_building']:
-        if text in get_buildings():
-            set_buffer(user_id, text)
+        if text in get_buildings(cursor):
+            set_buffer(user_id, text, cursor)
             answer = 'Выберите аудиторию или введите её номер'
             markup = markups.room_numbers('%', text)
             state = states['waiting_for_room']
     elif state == states['waiting_for_room']:
-        number_of_rooms = len(get_rooms(text, get_buffer(user_id)))
+        number_of_rooms = len(get_rooms(text, get_buffer(user_id, cursor), cursor))
         if number_of_rooms == 1:
-            if len(get_trackings(user_id)) == 0:
-                set_tracking(user_id, 'room', get_room_id(text, get_buffer(user_id)), True, text)
+            if len(get_trackings(user_id, cursor)) == 0:
+                set_tracking(user_id, 'room', get_room_id(text, get_buffer(user_id, cursor), cursor), True, text, cursor)
                 answer = f'''Выбрана аудитория:\n\n{text}\n\nБот может отсылать вам уведомления о предстоящих занятиях, есть три вида уведомлений:\n\n1)Уведомления перед каждой парой - такие уведомления будут приходить за выбранное время до начала пары\n\n2)Уведомление перед первой парой - для первой пары можно выставить отдельное время\n\n3)Уведомление о парах на день - позволяет получать расписание всех занятий на предстоящий день'''
                 bot.send_message(user_id, answer, reply_markup=markup)
                 answer = 'Уведомления перед каждой парой:'
                 markup = markups.every_lesson_notification()
                 state = states['every_lesson_notification']
             else:
-                set_tracking(user_id, 'room', get_room_id(text, get_buffer(user_id)), False, text)
+                set_tracking(user_id, 'room', get_room_id(text, get_buffer(user_id, cursor), cursor), False, text, cursor)
                 answer = f'''Выбрана аудитория:\n\n{text}\n\nТеперь в главном меню вы можете найти кнопку для вывода расписания для данной аудитории\n\nУведомления о парах приходят только для главного расписания, сделать расписание главным:\n\nНастройки → Главное расписание'''
                 bot.send_message(user_id, answer, reply_markup=markups.remove())
                 bot.send_chat_action(user_id, 'typing')
-                answer = get_schedule(user_id, main=True)
+                answer = get_schedule(user_id, main=True, cursor=cursor)
                 markup = markups.main()
         elif number_of_rooms > 1:
             answer = 'По запросу найдено несколько аудиторий:'
-            markup = markups.room_numbers(text, get_buffer(user_id))
+            markup = markups.room_numbers(text, get_buffer(user_id, cursor))
         else:
             answer = 'Неизвестная аудитория, попробуйте найти её в списке'
-            markup = markups.room_numbers('%', get_buffer(user_id))
+            markup = markups.room_numbers('%', get_buffer(user_id, cursor))
     elif state == states['every_lesson_notification']:
         if text in every_lesson_notification_options.keys():
             time = every_lesson_notification_options[text]
-            set_every_lesson_notification(user_id, time)
+            set_every_lesson_notification(user_id, time, cursor)
             if time is not None:
                 answer = f'Уведомление будет приходить {text.lower()}'
             else:
@@ -150,7 +152,7 @@ def handle_command(message):
             state = states['first_lesson_notification']
     elif state == states['first_lesson_notification']:
         if text.isdigit() and -1 < int(text) < 360:
-            set_first_lesson_notification(user_id, int(text) * 60)
+            set_first_lesson_notification(user_id, int(text) * 60, cursor)
             answer = f'Уведомление будет приходить за {text} '
             if int(text) % 10 == 1:
                 answer += 'минуту'
@@ -164,7 +166,7 @@ def handle_command(message):
             state = states['day_notification']
         elif text == 'Не уведомлять':
             bot.send_message(user_id, answer, reply_markup=markups.remove())
-            set_first_lesson_notification(user_id, None)
+            set_first_lesson_notification(user_id, None, cursor)
             answer = 'Укажите время, в которое присылать расписание на предстоящий день:'
             markup = markups.get_day_notification_inline()
             state = states['day_notification']
@@ -189,17 +191,18 @@ def handle_command(message):
     #                 markup = markups.main()
     #                 state = states['main']
     bot.send_message(user_id, answer, reply_markup=markup)
-    change_state(user_id, state)
+    change_state(user_id, state, cursor)
     print(f'{state} {user_id}: {text} | executed in {datetime.datetime.now() - start_time}')
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
+@connect_and_run
+def callback_query(call, cursor):
     text = call.message.text
     chat_id = call.message.chat.id
     message_id = call.message.message_id
     user_id = call.from_user.id
-    state = get_state(user_id)
+    state = get_state(user_id, cursor)
     markup = False
     edit = True
     if state == states['first_lesson_notification']:
@@ -212,18 +215,18 @@ def callback_query(call):
         if button == 'Submit' or button == 'Cancel':
             edit = False
             if button == 'Submit':
-                set_day_notification(user_id, f'{hours[0]}{hours[1]}:{minutes[0]}{minutes[1]}')
+                set_day_notification(user_id, f'{hours[0]}{hours[1]}:{minutes[0]}{minutes[1]}', cursor)
                 answer = f'Расписание будет приходить в {hours[0]}{hours[1]}:{minutes[0]}{minutes[1]}.\nВнимание: если к этому времени у вас уже будут идти пары, то придет расписание на следующий день'
             else:
                 edit = False
                 answer = 'Уведомление с расписанием на предстоящий день приходить не будет'
-            if get_buffer(user_id) == 'Настройки':
+            if get_buffer(user_id, cursor) == 'Настройки':
                 # TODO: go to settings
                 pass
             else:
                 bot.send_message(user_id, answer, reply_markup=markups.remove())
                 bot.send_chat_action(user_id, 'typing')
-                answer = get_schedule(user_id, main=True)
+                answer = get_schedule(user_id, main=True, cursor=cursor)
                 markup = markups.main()
                 state = states['main']
         else:
@@ -274,9 +277,9 @@ def callback_query(call):
     else:
         bot.send_message(user_id, answer, reply_markup=markups.remove())
         bot.send_chat_action(user_id, 'typing')
-        answer = get_schedule(user_id, main=True)
+        answer = get_schedule(user_id, main=True, cursor=cursor)
         markup = markups.main()
-        change_state(user_id, state)
+        change_state(user_id, state, cursor)
         bot.send_message(user_id, answer, reply_markup=markup)
 
 
