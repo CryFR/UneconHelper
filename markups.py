9142632945abcdfba905f12ed490421b69d86a5c
Vladislav <54751848+vermoouth@users.noreply.github.com>
2019-12-13 20:44:55 +0300

    Update markups.py

--- markups.py
+++ markups.py
@@ -1,9 +1,17 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton, \
+    ReplyKeyboardRemove
 from db_api import *
+from constants import every_lesson_notification_options
 
 
 def remove():
     mkp = ReplyKeyboardRemove()
+    return mkp
+
+
+def no_need():
+    mkp = ReplyKeyboardMarkup(True, False)
+    mkp.row('Не уведомлять')
     return mkp
 
 
@@ -29,11 +37,17 @@ def specialities(faculty):
     return mkp
 
 
-def groups(speciality):
+def groups(faculty, speciality):
     mkp = ReplyKeyboardMarkup(True, False, row_width=4)
     buttons = []
-    for group in get_groups(speciality):
-        buttons.append(KeyboardButton(group))
+    last_course = 0
+    for group in get_groups(faculty, speciality):
+        if group['course'] > last_course:
+            mkp.add(*buttons)
+            mkp.row(f"{group['course']} курс")
+            last_course = group['course']
+            buttons = []
+        buttons.append(KeyboardButton(group['number']))
     mkp.add(*buttons)
     return mkp
 
@@ -47,32 +61,67 @@ def teachers(name):
 
 def buildings():
     mkp = ReplyKeyboardMarkup(True, False)
-    for _building in get_buildings():\
+    for _building in get_buildings():
         mkp.row(_building)
     return mkp
 
 
-def room_numbers(number, _building):
+def room_numbers(number, building):
     mkp = ReplyKeyboardMarkup(True, False, row_width=5)
     buttons = []
-    for room in get_rooms(number, _building):
+    for room in get_rooms(number, building):
         buttons.append(room)
     mkp.add(*buttons)
     return mkp
 
 
-def lesson_notifications():
-    mkp = ReplyKeyboardMarkup(True, False)
-    mkp.row('️️За 5 минут', 'За 10 минут')
-    mkp.row('️️За 15 минут', 'Не уведомлять')
+def every_lesson_notification():
+    mkp = ReplyKeyboardMarkup(True, False, row_width=2)
+    for button in every_lesson_notification_options.keys():
+        mkp.add(KeyboardButton(button))
     return mkp
 
 
-def day_notifications():
-    mkp = ReplyKeyboardMarkup(True, False)
-    mkp.row('️️Вечером', 'Утром')
-    mkp.row('Не уведомлять')
+def main():
+    mkp = ReplyKeyboardMarkup(True, False, row_width=2)
+    mkp.row('Menu')
+    # TODO: menu markup
+    return mkp
+
+
+def get_day_notification_inline(hours=None, minutes=None):
+    if minutes is None:
+        minutes = [0, 0]
+    if hours is None:
+        hours = [0, 0]
+    mkp = InlineKeyboardMarkup()
+    data = f' {hours[0]} {hours[1]} {minutes[0]} {minutes[1]}'
+    timer_mkp = inline_timer(hours, minutes, data)
+    for row in timer_mkp:
+        mkp.row(*row)
+    mkp.row(InlineKeyboardButton('Не уведомлять', callback_data='Cancel' + data),
+            InlineKeyboardButton('Подтвердить', callback_data='Submit' + data))
+    return mkp
+
+
+def inline_timer(hours, minutes, data):
+    mkp = ((InlineKeyboardButton('▲', callback_data='h0+' + data),
+            InlineKeyboardButton('▲', callback_data='h1+' + data),
+            InlineKeyboardButton('-', callback_data='None' + data),
+            InlineKeyboardButton('▲', callback_data='m0+' + data),
+            InlineKeyboardButton('▲', callback_data='m1+' + data)),
+           (InlineKeyboardButton(hours[0], callback_data='None' + data),
+            InlineKeyboardButton(hours[1], callback_data='None' + data),
+            InlineKeyboardButton(':', callback_data='None' + data),
+            InlineKeyboardButton(minutes[0], callback_data='None' + data),
+            InlineKeyboardButton(minutes[1], callback_data='None' + data)),
+           (InlineKeyboardButton('▼', callback_data='h0-' + data),
+            InlineKeyboardButton('▼', callback_data='h1-' + data),
+            InlineKeyboardButton('-', callback_data='None' + data),
+            InlineKeyboardButton('▼', callback_data='m0-' + data),
+            InlineKeyboardButton('▼', callback_data='m1-' + data)))
+    return mkp
 
 
 if __name__ == '__main__':
-    print(building())
+    print(groups(get_buffer(294821600), 'Информационная безопасность'))
