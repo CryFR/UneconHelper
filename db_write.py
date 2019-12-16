0a93457cf2c7093a76a5e407cc39c8a682dedbc1
Vladislav <54751848+vermoouth@users.noreply.github.com>
2019-12-16 14:37:22 +0300

    Update db_write.py

--- db_write.py
+++ db_write.py
@@ -12,7 +12,7 @@ errors_found = 0
 
 def fill_faculties():
     keys_fac, values_fac = zip(*schedule_parser.get_faculties().items())
-    sql = "INSERT INTO `faculties` (`faculty_id`, `faculty_name_ru`, `courses`) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO `faculties` (`faculty_id`, `faculty_name`, `courses`) VALUES (%s, %s, %s)"
     for i in range(0, len(keys_fac)):
         data = (keys_fac[i], values_fac[i], ','.join((schedule_parser.get_courses(keys_fac[i]).keys())))
         try:
@@ -94,93 +94,12 @@ def dates_to_list(dates, period, even_odd='', date_format='%d.%m.%y'):
     return list_of_dates
 
 
-# def fill_groups(faculty_id, course):
-#     sql = "INSERT INTO `groups` (group_id, group_number, group_speciality_name, group_speciality_code, faculty_id, course) VALUES (%s, %s, %s, %s, %s, %s)"
-#     keys_grp, values_grp = zip(*schedule_parser.get_groups(faculty_id, course).items())
-#     for i in range(0, len(keys_grp)):
-#         data = (keys_grp[i], values_grp[i][0], values_grp[i][2], values_grp[i][1], faculty_id, course)
-#         try:
-#             cursor.execute(sql, data)
-#         except Exception as e:
-#             print(e)
-#         i += 1
-#
-#
-# def fill_teachers(group_id):
-#     sql = "INSERT INTO `teachers` (`surname_ru`, `first_name_ru`, `patronymic_ru`) VALUES (%s, %s, %s)"
-#     group_teachers = set(lesson['teacher']
-#                          for lesson in schedule_parser.parse_semester(group_id))
-#     for teacher in group_teachers:
-#         data = teacher.split()
-#         try:
-#             cursor.execute(sql, data)
-#         except Exception as e:
-#             print(e)
-#
-#
-# def fill_subjects(group_id):
-#     sql = "INSERT INTO `subjects` (`subject_name_ru`) VALUES (%s)"
-#     group_subjects = set(lesson['subject'] for lesson in schedule_parser.parse_semester(group_id))
-#     for subject in group_subjects:
-#         try:
-#             cursor.execute(sql, subject)
-#         except Exception as e:
-#             print(e)
-#
-#
-# def fill_rooms(group_id):
-#     sql = "INSERT INTO `rooms` (`number`, `building_ru`) VALUES (%s, %s)"
-#     group_rooms = {lesson['room']: lesson['building']
-#                    for lesson in schedule_parser.parse_semester(group_id)}
-#     for room in set(group_rooms):
-#         data = [room, group_rooms[room]]
-#         try:
-#             cursor.execute(sql, data)
-#         except Exception as e:
-#             print(e)
-#
-#
-#
-#
-# def fill_lessons(group_id):
-#     cursor.execute("SELECT `subject_id`, `subject_name_ru` FROM `subjects`")
-#     subjects = {subject_name: subject_id for subject_id, subject_name in cursor.fetchall()}
-#     cursor.execute("SELECT `teacher_id`, `surname_ru`, `first_name_ru`, `patronymic_ru` FROM teachers")
-#     teachers = {' '.join(full_name): teacher_id for teacher_id, *full_name in cursor.fetchall()}
-#     cursor.execute("SELECT `room_id`, `number`, `building_ru` FROM `rooms`")
-#     rooms = {' '.join(full_address): room_id for room_id, *full_address in cursor.fetchall()}
-#     cursor.execute("SELECT `number`, `time` FROM `timings`")
-#     timings = {time: number for number, time in cursor.fetchall()}
-#     data: list = []
-#     for lesson in schedule_parser.parse_semester(group_id):
-#         new_data = [subjects.get(lesson['subject'], 'NULL'),
-#                     teachers.get(lesson['teacher'], 'NULL'),
-#                     rooms.get(lesson['room'] + ' ' + lesson['building'], 'NULL'),
-#                     timings.get(lesson['time'], 'NULL'),
-#                     lesson.get('type', 'NULL'),
-#                     group_id
-#                     ]
-#         try:
-#             index = [row[:3] + row[4:] for row in data].index(new_data)
-#             data[index][3].extend(dates_to_list(lesson['dates'], lesson['even_odd']))
-#         except ValueError:
-#             new_data.insert(3, dates_to_list(lesson['dates'], lesson['even_odd']))
-#             data.append(new_data)
-#     sql = "INSERT INTO lessons (subject_id, teacher_id, room_id, dates, lesson_number, type_ru, group_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-#     for row in data:
-#         row[3] = dumps(row[3])
-#         try:
-#             cursor.execute(sql, row)
-#         except Exception as e:
-#             print(e)
-
-
 def group_fill_all(group_id):
-    cursor.execute("SELECT `subject_id`, `subject_name_ru` FROM `subjects`")
+    cursor.execute("SELECT `subject_id`, subject_name FROM `subjects`")
     subjects = {subject_name: subject_id for subject_id, subject_name in cursor.fetchall()}
-    cursor.execute("SELECT `teacher_id`, `surname_ru`, `first_name_ru`, `patronymic_ru` FROM teachers")
+    cursor.execute("SELECT `teacher_id`, `surname`, `first_name`, `patronymic` FROM teachers")
     teachers = {' '.join(full_name).strip(): teacher_id for teacher_id, *full_name in cursor.fetchall()}
-    cursor.execute("SELECT `room_id`, `number`, `building_ru` FROM `rooms`")
+    cursor.execute("SELECT `room_id`, `number`, `building` FROM `rooms`")
     rooms = {' '.join(full_address).strip(): room_id for room_id, *full_address in cursor.fetchall()}
     cursor.execute("SELECT `number`, `time` FROM `timings`")
     timings = {time: number for number, time in cursor.fetchall()}
@@ -193,18 +112,18 @@ def group_fill_all(group_id):
                 surname = first_name = patronymic = ''
                 surname, first_name, *patronymic = lesson['teacher'].split()
                 cursor.execute(
-                    "INSERT INTO `teachers` (`teacher_id`, `surname_ru`, `first_name_ru`, `patronymic_ru`) VALUES (%s, %s, %s, %s)",
+                    "INSERT INTO `teachers` (`teacher_id`, `surname`, `first_name`, `patronymic`) VALUES (%s, %s, %s, %s)",
                     [lesson['teacher_id'], surname, first_name, ' '.join(patronymic)])
-                cursor.execute("SELECT `teacher_id`, `surname_ru`, `first_name_ru`, `patronymic_ru` FROM teachers")
+                cursor.execute("SELECT `teacher_id`, `surname`, `first_name`, `patronymic` FROM teachers")
                 teachers = {' '.join(full_name).strip(): teacher_id for teacher_id, *full_name in cursor.fetchall()}
             if lesson['subject'] not in subjects.keys():
-                cursor.execute("INSERT INTO `subjects` (`subject_name_ru`) VALUES (%s)", lesson['subject'])
-                cursor.execute("SELECT `subject_id`, `subject_name_ru` FROM `subjects`")
+                cursor.execute("INSERT INTO `subjects` (subject_name) VALUES (%s)", lesson['subject'])
+                cursor.execute("SELECT `subject_id`, subject_name FROM `subjects`")
                 subjects = {subject_name: subject_id for subject_id, subject_name in cursor.fetchall()}
             if ' '.join([lesson['room'], lesson['building']]).strip() not in rooms.keys():
-                cursor.execute("INSERT INTO `rooms` (`number`, `building_ru`) VALUES (%s, %s)",
+                cursor.execute("INSERT INTO `rooms` (`number`, `building`) VALUES (%s, %s)",
                                [lesson['room'], lesson['building']])
-                cursor.execute("SELECT `room_id`, `number`, `building_ru` FROM `rooms`")
+                cursor.execute("SELECT `room_id`, `number`, `building` FROM `rooms`")
                 rooms = {' '.join(full_address).strip(): room_id for room_id, *full_address in cursor.fetchall()}
             new_lesson_data = [subjects.get(lesson['subject'], ''),
                                lesson['is_choosable'],
